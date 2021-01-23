9d2f2b1a0b1fd141af863f0bed05192447ce62a4
TheHamkerCat <thehamkercat@gmail.com>
2021-01-24 01:52:40 +0530

    only send top 10 karma

--- karma.py
+++ karma.py
@@ -97,21 +97,25 @@ async def downvote(_, message):
         f'Decremented Karma Of {user_mention} By 1 \nTotal Points: {members[f"{user_id}"]}'
     )
 
-
+import operator
 @app.on_message(filters.command(["karma"]) & filters.group)
 async def karma(_, message):
     chat_id = message.chat.id
     filename = f"{chat_id}.json"
     with open(filename) as f2:
         members = json.load(f2)
+    fmembers = dict(sorted(members.items(), key=operator.itemgetter(1),reverse=True))
+    await message.reply_text(len(fmembers))
     if not message.reply_to_message:
         output = ""
         m = 0
-        for i in members.keys():
+        for i in fmembers.keys():
             try:
-                output += f"`{(await app.get_chat(i)).username}: {list(members.values())[m]}`\n"
+                output += f"`{(await app.get_chat(i)).username}: {list(fmembers.values())[m]}`\n"
             except:
                 pass
+            if m == 10:
+                break
             m += 1
         await message.reply_text(output)
 
